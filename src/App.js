218b1e1692bab6bf6e28696706644479b20cac58
Rohit verma <28vrohit@gmail.com>
2022-09-02 01:22:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import FirstCarousel from "./Components/FirstCarousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <FirstCarousel/>
       <Footer/>
     </div>
   );
---
+++ src/Components/FirstCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const firstCarousel = () => {
+  return (
+    <div>firstCarousel</div>
+  )
+}
+
+export default firstCarousel
