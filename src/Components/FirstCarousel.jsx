f74a3247849f58244b54eb254b317de5d3b4c256
Rohit verma <28vrohit@gmail.com>
2022-09-03 01:06:44 +0530

    Carousel

--- src/Components/FirstCarousel.jsx
+++ src/Components/FirstCarousel.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from "../Image/img1.jpeg";
+import img2 from "../Image/img2.jpeg";
+import img3 from "../Image/img3.jpeg";
+
 
 const firstCarousel = () => {
   return (
-    <div>firstCarousel</div>
+    <Carousel fade controls={false}>
+      <Carousel.Item interval={500}>
+        <img
+          height="288" width="388"
+          className="d-block w-100"
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          height="288" width="388"
+          className="d-block w-100"
+          src={img2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          height="288" width="388"
+          className="d-block w-100"
+          src={img3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   )
 }
 
